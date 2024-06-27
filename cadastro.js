ff1d50e86eeb152d32b2b732ca2b86c0c1595979
paulalima93 <91716314+paulalima93@users.noreply.github.com>
2024-06-27 19:44:37 -0300

    Update cadastro.js

--- cadastro.js
+++ cadastro.js
@@ -4,7 +4,14 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/fireba
 import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
 // Your web app's Firebase configuration
-
+const firebaseConfig = {
+  apiKey: "AIzaSyBtqgilbgTdyokdHschkugRovMSs6K22Pw",
+  authDomain: "app-login-f2202.firebaseapp.com",
+  projectId: "app-login-f2202",
+  storageBucket: "app-login-f2202.appspot.com",
+  messagingSenderId: "694134959047",
+  appId: "1:694134959047:web:eff1fdcbf86271137d07e2"
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
@@ -18,7 +25,18 @@ document.getElementById("login_button").addEventListener("click", function(){
   //pegar confirmação da senha digitada na caixa de confirmar senha e armazenar na const confirmeSenha
     
     if (senha == confirmeSenha) {
+      createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed up 
+        const user = userCredential.user;
+        console.log("Deu certo!");
         
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // ..
+      });
     } else {
         document.getElementById("resultado").innerHTML = " ";
         document.getElementById("resultado").style.color = "red";
